b939ed3eab60bb54eeba3b534954bde3619af0a9
Maurizio Strippoli <phokingmau@mac.mynetworksettings.com>
2026-06-10 22:16:53 -0400

    Add OOP exercises

---
+++ DUAD/OOP/OOP Extra/4pilaresOOP-extra/Ejercicio2-user.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+
+
+class User(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def get_role(self):
+        pass
+
+    @abstractmethod
+    def has_permission(self, permission):
+        pass
+
+
+class AdminUser(User):
+    def get_role(self):
+        return "Admin"
+
+    def has_permission(self, permission):
+        return True
+
+
+class RegularUser(User):
+    def get_role(self):
+        return "Regular"
+
+    def has_permission(self, permission):
+        return permission == "read"
+
+
+# Test
+user1 = AdminUser("Carlos")
+user2 = RegularUser("Andrea")
+print(user1.has_permission("delete"))  # True
+print(user2.has_permission("delete"))  # False
